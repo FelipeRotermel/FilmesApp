1a0447d5fd975437a0a0694872d2a48aeaa18e5b
FelipeRotermel <feliperotermell@gmail.com>
2023-03-27 16:05:00 -0300

    Pronto

--- App.js
+++ App.js
@@ -1,39 +1,62 @@
-import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
 
-  function Card(props){
-    return(
-      <View style={styles.filmes}>
-        <Image style={styles.imagens} source={{uri: props.filme.imagem}} />
-        <Text style={styles.titulo}>{props.filme.titulo}</Text>
-        <Text style={styles.texto}>{props.filme.genero}</Text>
-      </View>
-    )
-  }
-
-  const filmes = [ 
+  const [filmes, setFilmes] = useState([ 
     {
+      id: 1,
       titulo: 'Cocaine Bear', 
       genero: 'Ficção', 
       imagem: 'https://m.media-amazon.com/images/M/MV5BODAwZDQ5ZjEtZDI1My00MTFiLTg0ZjUtOGE2YTBkOTdjODFhXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg',
     },
     {
+      id: 2,
       titulo: 'Rubber, O Pneu Assassino',
       genero: 'Terror', 
       imagem: 'https://www.cafecomfilme.com.br/media/k2/items/cache/e027c4032dd7e9a75b2ece7beabff93c_XL.jpg?t=20170825_164600',
     },
     {
-      titulo: 'Cocaine Bear', 
-      genero: 'Ficção', 
-      imagem: 'https://m.media-amazon.com/images/M/MV5BODAwZDQ5ZjEtZDI1My00MTFiLTg0ZjUtOGE2YTBkOTdjODFhXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg',
+      id: 3,
+      titulo: 'Onde os fracos não têm vez', 
+      genero: 'Suspense, Drama', 
+      imagem: 'https://br.web.img3.acsta.net/pictures/210/079/21007978_20130523175557996.jpg',
     },
-  ]
+    {
+      id: 4,
+      titulo: 'Click',
+      genero: 'Comédia, Drama',
+      imagem: 'https://www.cafecomfilme.com.br/media/k2/items/cache/99a55ddf297fe1c0e5cd25b32402e97a_XL.jpg?t=20200716_212331',
+    },
+    {
+      id: 5,
+      titulo: 'Ataque Do Tubarão De 5 Cabeças',
+      genero: 'Terror',
+      imagem: 'https://br.web.img2.acsta.net/pictures/22/04/28/18/27/3434343.png',
+    },
+  ]);
+
+  function Card(props){
+    function excluirFilme() {
+      setFilmes(filmes.filter(filme => filme.id !== props.filme.id));
+    }
+    return(
+      <View style={styles.filmes}>
+        <Image style={styles.imagens} source={{uri: props.filme.imagem}} />
+        <Text style={styles.titulo}>{props.filme.titulo}</Text>
+        <Text style={styles.texto}>{props.filme.genero}</Text>
+        <TouchableOpacity onPress={excluirFilme}>
+          <Image style={styles.lixo} source={{uri: 'https://cdn-icons-png.flaticon.com/512/2891/2891491.png'}} />
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.nome}>Filmes</Text>
       <ScrollView horizontal pagingEnabled>
-        {filmes.map(filme => <Card filme={filme} />)}
+        {filmes.map(filme => <Card key={filme.id} filme={filme} />)}
       </ScrollView>
     </View>
   );
@@ -64,7 +87,8 @@ const styles = StyleSheet.create({
   },
   filmes: {
     width: 150,
-    height: 350,
+    height: 370,
+    paddingBottom: 10,
     backgroundColor: '#e6faf8',
     justifyContent: 'center', 
     borderRadius: 10,
@@ -80,21 +104,31 @@ const styles = StyleSheet.create({
   },
   imagens: {
     maxWidth: 150,
-    height: 250,
-    borderTopRightRadius: 10,
-    borderTopLeftRadius: 10,
-    resizeMode: 'contain' ,
+    height: 270,
+    borderTopRightRadius: 5,
+    borderTopLeftRadius: 5,
+    resizeMode: 'stretch',
   },
   titulo: {
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 10,
+    height: 45,
+    marginLeft: 5,
   },
   texto: {
     textAlign: 'left',
-    height: 50,
+    height: 40,
     fontSize: 19,
     color: 'blue',
     marginLeft: 5,
+    height: 50,
+  },
+  lixo: {
+    width: 25,
+    height: 25,
+    position: 'absolute',
+    right: 5,
+    bottom: 1,
   },
 });
